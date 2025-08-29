0f5f985df869d2a154a01a7f2f87e1a873ff8a8f
Turjo101365 <acd776959@gmail.com>
2025-08-29 16:49:56 -0700

    Cluster Name Changed

--- src/backend/db.js
+++ src/backend/db.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Turjo:Test123@cluster0.nyf4dh7.mongodb.net/', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://Turjo:Test123@cluster0.dmtticy.mongodb.net/retryWrites=true&w=majority&appName=Cluster0'
+)
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
